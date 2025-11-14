d0f48a0d71acda5580d743a5b095ea6b5e65bc08
tuankhoi06 <tuankhoi06.hg@gmail.com>
2025-11-14 07:47:43 +0700

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+﻿use Quanlybenhvien
+INSERT INTO BENHNHAN (MaBenhNhan, HoTen, NgaySinh, GioiTinh, DiaChi, SoDienThoai, TrangThaiBenhNhan) 
+VALUES
+(1, N'Nguyễn Đoàn Huy', '1980-02-15', 'M', N'Ha Noi', '0987654321', N'Đang điều trị'),
+(2, N'Phạm Thị Mai', '1986-07-22', 'F', N'TP HCM', '0932123456', N'Đang điều trị'),
+(3, N'Tran Văn Khoa', '1984-01-15', 'M', N'Da Nang', '0912345678', N'Đã khỏi bệnh'),
+(4, N'Nguyễn Ngọc Anh', '1991-12-14', 'M', N'Ha Noi', '0909876543', N'Đang điều trị'),
+(5, N'Lê Minh Duy', '1992-03-18', 'M', N'Bình Dương', '0934234567', N'Chưa khám'),
+(6, N'Hàm Thái Bình', '1983-05-10', 'M', N'TP HCM', '0943234567', N'Đang điều trị'),
+(7, N'Huỳnh Quang Duy', '1987-10-01', 'M', N'Đà Nẵng', '0965234567', N'Đã khỏi bệnh'),
+(8, N'Nguyễn Thị Liên', '1980-01-15', 'F', N'Hải Phòng', '0975234567', N'Chưa khám'),
+(9, N'Nguyễn Văn Quang', '1984-08-22', 'M', N'TP HCM', '0986234567', N'Đang điều trị'),
+(10, N'Lê Hữu Yên', '1992-06-29', 'F', N'Hà Nội', '0906123456', N'Chưa khám');
+
