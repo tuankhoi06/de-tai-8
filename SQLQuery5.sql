fea7557778d5ef1dba4a2cad72aac22089272f17
tuankhoi06 <tuankhoi06.hg@gmail.com>
2025-11-14 08:28:51 +0700

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO PHONGKHAM (MaPhong, TenPhong, ChuyenKhoa, TinhTrang)
+VALUES
+(1, N'Phong A', N'Khoa Nhĩ', N'Đã có bệnh nhân'),
+(2, N'Phong B', N'Nhi khoa', N'Đã có bệnh nhân'),
+(3, N'Phong C', N'Sản khoa', N'Còn trống'),
+(4, N'Phong D', N'Ngoại khoa', N'Còn trống'),
+(5, N'Phong E', N'Tai Mũi Họng', N'Đã có bệnh nhân'),
+(6, N'Phong F', N'Răng Hàm Mặt', N'Còn trống'),
+(7, N'Phong G', N'Phẫu thuật', N'Đã có bệnh nhân'),
+(8, N'Phong H', N'Khoa tim mạch', N'Còn trống'),
+(9, N'Phong I', N'Ngoại thần kinh', N'Đã có bệnh nhân'),
+(10, N'Phong J', N'Chuyên khoa tim mạch', N'Còn trống');
