ff0e227b51805632663df33ea8426ff785146ed8
Huydayne282 <bin389975@gmail.com>
2025-11-14 08:23:41 +0700

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,20 @@
+﻿INSERT INTO BacSi (MaBacSi, HoTen, NgaySinh, GioiTinh, DiaChi, ChuyenKhoa)
+VALUES
+(1, N'Nguyễn Văn A', '1980-02-15', 'M', N'Ha Noi', N'Khoa Nhĩ'),
+(2, N'Nguyễn Thị B', '1986-07-22', 'F', N'TP HCM', N'Nhĩ khoa'),
+(3, N'Trần Hoàng C', '1984-01-15', 'M', N'Da Nang', N'Sản khoa'),
+(4, N'Lê Hoàng D', '1991-12-14', 'M', N'Ha Noi', N'Ngoại khoa'),
+(5, N'Phan Thị E', '1992-03-18', 'F', N'Bình Dương', N'Tai Mũi Họng'),
+(6, N'Đặng Văn F', '1983-05-10', 'M', N'TP HCM', N'Răng Hàm Mặt'),
+(7, N'Huỳnh Quang G', '1987-10-01', 'M', N'Đà Nẵng', N'Phẫu thuật'),
+(8, N'Ngô Minh H', '1980-01-15', 'F', N'Hải Phòng', N'Khoa tim mạch'),
+(9, N'Thái Vani I', '1985-10-10', 'M', N'TP HCM', N'Chuyên khoa tim mạch'),
+(10, N'Vũ Thanh J', '1992-06-29', 'F', N'Hà Nội', N'Chuyên khoa tim mạch'),
+(11, N'Bùi Thị K', '1981-03-12', 'F', N'Hà Nội', N'Nội tổng quát'),
+(12, N'Tô Đình L', '1985-09-21', 'M', N'TP HCM', N'Nhĩ khoa'),
+(13, N'Nguyễn Pham M', '1979-11-05', 'M', N'Đà Nẵng', N'Ngoại tiêu hóa'),
+(14, N'Hoàng Thị N', '1984-07-12', 'F', N'Hải Phòng', N'Đa khoa'),
+(15, N'Phan Lê O', '1982-06-30', 'M', N'Nam Định', N'Đa Khoa'),
+(16, N'Vũ Thị Lan Hương', '1981-01-25', 'F', N'Bắc Ninh', N'Tai Mũi Họng'),
+(17, N'Hoàng Anh Dũng', '1983-08-14', 'M', N'Quảng Ninh', N'Răng Hàm Mặt'),
+(18, N'Ngô Thị Mai Anh', '1988-12-02', 'F', N'TP HCM', N'Đa Khoa');
